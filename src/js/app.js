ad586b53b91cecd158a6e3e78166e22b9991209c
Patricia-Berges <patry37.zgz@gmail.com>
2024-08-21 18:06:07 +0000

    Finished

--- src/js/app.js
+++ src/js/app.js
@@ -28,19 +28,30 @@ function render(variables = {}) {
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
-
+  let profile_name = variables.name == null ? `(Name)` : variables.name;
+  let profile_surname =
+    variables.lastName == null ? `(Last Name)` : variables.lastName;
+  let profile_role = variables.role == null ? "" : variables.role;
+  let profile_city = variables.city == null ? "" : variables.city;
+  let profile_country = variables.country == null ? "" : variables.country;
+  let comma = variables.city != null && variables.country != null ? `,` : "";
+  let profile_twitter = variables.twitter == null ? "" : variables.twitter;
+  let profile_github = variables.github == null ? "" : variables.github;
+  let profile_linkedin = variables.linkedin == null ? "" : variables.linkedin;
+  let profile_instagram =
+    variables.instagram == null ? "" : variables.instagram;
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${profile_name} ${profile_surname}</h1>
+          <h2>${profile_role}</h2>
+          <h3>${profile_city}${comma} ${profile_country}</h3>
+          <ul class="${variables.socialMediaPosition}">
+            <li><a href="https://twitter.com/${profile_twitter}"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${profile_github}"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/school/${profile_linkedin}"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${profile_instagram}"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
