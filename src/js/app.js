f07f946cc6f366bed2577093b088645ead10604c
Patricia-Berges <patry37.zgz@gmail.com>
2024-08-21 18:08:57 +0000

    Fixed linkedin link

--- src/js/app.js
+++ src/js/app.js
@@ -50,7 +50,7 @@ function render(variables = {}) {
           <ul class="${variables.socialMediaPosition}">
             <li><a href="https://twitter.com/${profile_twitter}"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/${profile_github}"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/${profile_linkedin}"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://linkedin.com/${profile_linkedin}"><i class="fab fa-linkedin"></i></a></li>
             <li><a href="https://instagram.com/${profile_instagram}"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
